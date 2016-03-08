c1059279d6d1bddbc432bcfc1ef25527c7992eaa
Vova Dranyy <v.dranyy@gmail.com>
2016-03-08 16:07:10 +0200

    Add basic view that returns minimal HTML.

---
+++ notes/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from notes.views import home_page
+from django.http import HttpRequest
+
+
+class HomePageTestCase(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Fisherman Diary</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
